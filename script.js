84d442ec2ab6450886f3e955f3dd8a877eec5039
HARSH RAJ <129783128+harsh10009@users.noreply.github.com>
2024-08-20 00:45:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function run()
 {
     let htmlCode = document.getElementById("html-code").value;
     let cssCode = document.getElementById("css-code").value;
-    let jsCcode = document.getElementById("js-code").value;
+    let jsCode = document.getElementById("js-code").value;
     let output = document.getElementById("out-code");
 
     output.contentDocument.body.innerHTML = htmlCode + "<style>" +cssCode+ "</style>";
